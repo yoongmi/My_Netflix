3663f3cdc0b8073abbb3406cfe4532d6f17ad29d
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-11-15 11:16:46 +0900

    home redirect

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -3,6 +3,7 @@ import Header from "../routes/Header";
 import Movie from "../routes/Movie";
 import Search from "../routes/Search";
 import Tv from "../routes/Tv";
+import Home from "../routes/Home";
 import Footer from "./Footer";
 
 const HomeRouter = () => {
@@ -11,11 +12,14 @@ const HomeRouter = () => {
       <Router>
         <Header />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/movie" element={<Movie />} />
           <Route path="/movie/:category/:movieId" element={<Movie />} />
           <Route path="/tv" element={<Tv />} />
           <Route path="/tv/:category/:movieId" element={<Tv />} />
           <Route path="/search" element={<Search />} />
+          <Route path="/search/:category/:movieId" element={<Search />} />
+          <Route path="/tv/search2/:movieId" element={<Search />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/routes/Home.tsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <Navigate to="/movie"></Navigate>
+    </>
+  );
+};
+export default Home;
