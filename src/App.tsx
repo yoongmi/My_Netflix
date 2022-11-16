549f0184d5abc036f7aa551bb5d91c98f947b791
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-11-16 16:02:21 +0900

    모바일지원불가 텍스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import { darkTheme, lightTheme } from "./theme";
 import HomeRouter from "./components/router";
 import { useRecoilValue } from "recoil";
 import { ThemeAtom } from "./atom";
+import { useEffect, useState } from "react";
 
 function App() {
   const isTheme = useRecoilValue(ThemeAtom);
+  const [windowWidth, setwindowWidth] = useState(window.outerWidth);
+  const windowSize = () => {
+    setwindowWidth(window.outerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", windowSize);
+    return () => {
+      window.removeEventListener("resize", windowSize);
+    };
+  }, [window.outerWidth]);
   return (
     <ThemeProvider theme={isTheme ? lightTheme : darkTheme}>
       <GlobalStyle />
       <HomeRouter />
+      {windowWidth < 768 && (
+        <Sizeblack>
+          <p>
+            이 페이지는 모바일에서 지원하지 않습니다.
+            <br />
+            PC 화면을 확인해주세요.
+          </p>
+        </Sizeblack>
+      )}
     </ThemeProvider>
   );
 }
+const Sizeblack = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) => props.theme.bgDark};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  p {
+    font-size: 20px;
+    color: ${(props) => props.theme.txtColor};
+    line-height: 1.4;
+  }
+`;
 
 const GlobalStyle = createGlobalStyle`
 /* http://meyerweb.com/eric/tools/css/reset/ 
